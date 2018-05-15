16aae8c2114035bab95d5abb9b796d54b212b9dc
Milvis <info@milvis.lt>
2018-05-15 21:58:31 +0300

    tel

---
+++ _2018_03_16_pirma_uzduotis/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace _2018_03_16_pirma_uzduotis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generate = new System.Windows.Forms.Button();
+            this.textM1 = new System.Windows.Forms.TextBox();
+            this.textM2 = new System.Windows.Forms.TextBox();
+            this.textM3 = new System.Windows.Forms.TextBox();
+            this.textM4 = new System.Windows.Forms.TextBox();
+            this.textM5 = new System.Windows.Forms.TextBox();
+            this.textJ5 = new System.Windows.Forms.TextBox();
+            this.textJ4 = new System.Windows.Forms.TextBox();
+            this.textJ3 = new System.Windows.Forms.TextBox();
+            this.textJ2 = new System.Windows.Forms.TextBox();
+            this.textJ1 = new System.Windows.Forms.TextBox();
+            this.textR5 = new System.Windows.Forms.TextBox();
+            this.textR4 = new System.Windows.Forms.TextBox();
+            this.textR3 = new System.Windows.Forms.TextBox();
+            this.textR2 = new System.Windows.Forms.TextBox();
+            this.textR1 = new System.Windows.Forms.TextBox();
+            this.textG5 = new System.Windows.Forms.TextBox();
+            this.textG4 = new System.Windows.Forms.TextBox();
+            this.textG3 = new System.Windows.Forms.TextBox();
+            this.textG2 = new System.Windows.Forms.TextBox();
+            this.textG1 = new System.Windows.Forms.TextBox();
+            this.textZ5 = new System.Windows.Forms.TextBox();
+            this.textZ4 = new System.Windows.Forms.TextBox();
+            this.textZ3 = new System.Windows.Forms.TextBox();
+            this.textZ2 = new System.Windows.Forms.TextBox();
+            this.textZ1 = new System.Windows.Forms.TextBox();
+            this.bPlay = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.isvedimas = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(421, 21);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(102, 42);
+            this.Generate.TabIndex = 0;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // textM1
+            // 
+            this.textM1.Location = new System.Drawing.Point(30, 31);
+            this.textM1.Name = "textM1";
+            this.textM1.Size = new System.Drawing.Size(30, 22);
+            this.textM1.TabIndex = 1;
+            // 
+            // textM2
+            // 
+            this.textM2.Location = new System.Drawing.Point(30, 73);
+            this.textM2.Name = "textM2";
+            this.textM2.Size = new System.Drawing.Size(30, 22);
+            this.textM2.TabIndex = 2;
+            // 
+            // textM3
+            // 
+            this.textM3.Location = new System.Drawing.Point(30, 114);
+            this.textM3.Name = "textM3";
+            this.textM3.Size = new System.Drawing.Size(30, 22);
+            this.textM3.TabIndex = 3;
+            // 
+            // textM4
+            // 
+            this.textM4.Location = new System.Drawing.Point(30, 151);
+            this.textM4.Name = "textM4";
+            this.textM4.Size = new System.Drawing.Size(30, 22);
+            this.textM4.TabIndex = 4;
+            // 
+            // textM5
+            // 
+            this.textM5.Location = new System.Drawing.Point(30, 193);
+            this.textM5.Name = "textM5";
+            this.textM5.Size = new System.Drawing.Size(30, 22);
+            this.textM5.TabIndex = 5;
+            // 
+            // textJ5
+            // 
+            this.textJ5.Location = new System.Drawing.Point(76, 193);
+            this.textJ5.Name = "textJ5";
+            this.textJ5.Size = new System.Drawing.Size(28, 22);
+            this.textJ5.TabIndex = 10;
+            // 
+            // textJ4
+            // 
+            this.textJ4.Location = new System.Drawing.Point(76, 151);
+            this.textJ4.Name = "textJ4";
+            this.textJ4.Size = new System.Drawing.Size(28, 22);
+            this.textJ4.TabIndex = 9;
+            // 
+            // textJ3
+            // 
+            this.textJ3.Location = new System.Drawing.Point(76, 114);
+            this.textJ3.Name = "textJ3";
+            this.textJ3.Size = new System.Drawing.Size(28, 22);
+            this.textJ3.TabIndex = 8;
+            // 
+            // textJ2
+            // 
+            this.textJ2.Location = new System.Drawing.Point(76, 73);
+            this.textJ2.Name = "textJ2";
+            this.textJ2.Size = new System.Drawing.Size(28, 22);
+            this.textJ2.TabIndex = 7;
+            // 
+            // textJ1
+            // 
+            this.textJ1.Location = new System.Drawing.Point(76, 30);
+            this.textJ1.Name = "textJ1";
+            this.textJ1.Size = new System.Drawing.Size(28, 22);
+            this.textJ1.TabIndex = 6;
+            // 
+            // textR5
+            // 
+            this.textR5.Location = new System.Drawing.Point(122, 193);
+            this.textR5.Name = "textR5";
+            this.textR5.Size = new System.Drawing.Size(29, 22);
+            this.textR5.TabIndex = 15;
+            // 
+            // textR4
+            // 
+            this.textR4.Location = new System.Drawing.Point(122, 151);
+            this.textR4.Name = "textR4";
+            this.textR4.Size = new System.Drawing.Size(29, 22);
+            this.textR4.TabIndex = 14;
+            // 
+            // textR3
+            // 
+            this.textR3.Location = new System.Drawing.Point(122, 114);
+            this.textR3.Name = "textR3";
+            this.textR3.Size = new System.Drawing.Size(29, 22);
+            this.textR3.TabIndex = 13;
+            // 
+            // textR2
+            // 
+            this.textR2.Location = new System.Drawing.Point(122, 73);
+            this.textR2.Name = "textR2";
+            this.textR2.Size = new System.Drawing.Size(29, 22);
+            this.textR2.TabIndex = 12;
+            // 
+            // textR1
+            // 
+            this.textR1.Location = new System.Drawing.Point(122, 30);
+            this.textR1.Name = "textR1";
+            this.textR1.Size = new System.Drawing.Size(29, 22);
+            this.textR1.TabIndex = 11;
+            // 
+            // textG5
+            // 
+            this.textG5.Location = new System.Drawing.Point(167, 193);
+            this.textG5.Name = "textG5";
+            this.textG5.Size = new System.Drawing.Size(27, 22);
+            this.textG5.TabIndex = 20;
+            // 
+            // textG4
+            // 
+            this.textG4.Location = new System.Drawing.Point(167, 151);
+            this.textG4.Name = "textG4";
+            this.textG4.Size = new System.Drawing.Size(27, 22);
+            this.textG4.TabIndex = 19;
+            // 
+            // textG3
+            // 
+            this.textG3.Location = new System.Drawing.Point(167, 114);
+            this.textG3.Name = "textG3";
+            this.textG3.Size = new System.Drawing.Size(27, 22);
+            this.textG3.TabIndex = 18;
+            // 
+            // textG2
+            // 
+            this.textG2.Location = new System.Drawing.Point(167, 73);
+            this.textG2.Name = "textG2";
+            this.textG2.Size = new System.Drawing.Size(27, 22);
+            this.textG2.TabIndex = 17;
+            // 
+            // textG1
+            // 
+            this.textG1.Location = new System.Drawing.Point(167, 31);
+            this.textG1.Name = "textG1";
+            this.textG1.Size = new System.Drawing.Size(27, 22);
+            this.textG1.TabIndex = 16;
+            // 
+            // textZ5
+            // 
+            this.textZ5.Location = new System.Drawing.Point(214, 193);
+            this.textZ5.Name = "textZ5";
+            this.textZ5.Size = new System.Drawing.Size(29, 22);
+            this.textZ5.TabIndex = 25;
+            this.textZ5.TextChanged += new System.EventHandler(this.textBox20_TextChanged);
+            // 
+            // textZ4
+            // 
+            this.textZ4.Location = new System.Drawing.Point(214, 151);
+            this.textZ4.Name = "textZ4";
+            this.textZ4.Size = new System.Drawing.Size(29, 22);
+            this.textZ4.TabIndex = 24;
+            this.textZ4.TextChanged += new System.EventHandler(this.textBox21_TextChanged);
+            // 
+            // textZ3
+            // 
+            this.textZ3.Location = new System.Drawing.Point(214, 114);
+            this.textZ3.Name = "textZ3";
+            this.textZ3.Size = new System.Drawing.Size(29, 22);
+            this.textZ3.TabIndex = 23;
+            this.textZ3.TextChanged += new System.EventHandler(this.textBox22_TextChanged);
+            // 
+            // textZ2
+            // 
+            this.textZ2.Location = new System.Drawing.Point(214, 73);
+            this.textZ2.Name = "textZ2";
+            this.textZ2.Size = new System.Drawing.Size(29, 22);
+            this.textZ2.TabIndex = 22;
+            this.textZ2.TextChanged += new System.EventHandler(this.textBox23_TextChanged);
+            // 
+            // textZ1
+            // 
+            this.textZ1.Location = new System.Drawing.Point(214, 31);
+            this.textZ1.Name = "textZ1";
+            this.textZ1.Size = new System.Drawing.Size(29, 22);
+            this.textZ1.TabIndex = 21;
+            this.textZ1.TextChanged += new System.EventHandler(this.textZ1_TextChanged);
+            // 
+            // bPlay
+            // 
+            this.bPlay.Location = new System.Drawing.Point(429, 277);
+            this.bPlay.Name = "bPlay";
+            this.bPlay.Size = new System.Drawing.Size(125, 43);
+            this.bPlay.TabIndex = 26;
+            this.bPlay.Text = "Play";
+            this.bPlay.UseVisualStyleBackColor = true;
+            this.bPlay.Click += new System.EventHandler(this.bPlay_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Generate);
+            this.groupBox1.Controls.Add(this.textM1);
+            this.groupBox1.Controls.Add(this.textZ5);
+            this.groupBox1.Controls.Add(this.textM2);
+            this.groupBox1.Controls.Add(this.textZ4);
+            this.groupBox1.Controls.Add(this.textM3);
+            this.groupBox1.Controls.Add(this.textZ3);
+            this.groupBox1.Controls.Add(this.textM4);
+            this.groupBox1.Controls.Add(this.textZ2);
+            this.groupBox1.Controls.Add(this.textM5);
+            this.groupBox1.Controls.Add(this.textZ1);
+            this.groupBox1.Controls.Add(this.textJ1);
+            this.groupBox1.Controls.Add(this.textG5);
+            this.groupBox1.Controls.Add(this.textJ2);
+            this.groupBox1.Controls.Add(this.textG4);
+            this.groupBox1.Controls.Add(this.textJ3);
+            this.groupBox1.Controls.Add(this.textG3);
+            this.groupBox1.Controls.Add(this.textJ4);
+            this.groupBox1.Controls.Add(this.textG2);
+            this.groupBox1.Controls.Add(this.textJ5);
+            this.groupBox1.Controls.Add(this.textG1);
+            this.groupBox1.Controls.Add(this.textR1);
+            this.groupBox1.Controls.Add(this.textR5);
+            this.groupBox1.Controls.Add(this.textR2);
+            this.groupBox1.Controls.Add(this.textR4);
+            this.groupBox1.Controls.Add(this.textR3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 247);
+            this.groupBox1.TabIndex = 27;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ticket";
+            // 
+            // isvedimas
+            // 
+            this.isvedimas.Location = new System.Drawing.Point(16, 265);
+            this.isvedimas.Multiline = true;
+            this.isvedimas.Name = "isvedimas";
+            this.isvedimas.Size = new System.Drawing.Size(388, 246);
+            this.isvedimas.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 523);
+            this.Controls.Add(this.isvedimas);
+            this.Controls.Add(this.bPlay);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.TextBox textM1;
+        private System.Windows.Forms.TextBox textM2;
+        private System.Windows.Forms.TextBox textM3;
+        private System.Windows.Forms.TextBox textM4;
+        private System.Windows.Forms.TextBox textM5;
+        private System.Windows.Forms.TextBox textJ5;
+        private System.Windows.Forms.TextBox textJ4;
+        private System.Windows.Forms.TextBox textJ3;
+        private System.Windows.Forms.TextBox textJ2;
+        private System.Windows.Forms.TextBox textJ1;
+        private System.Windows.Forms.TextBox textR5;
+        private System.Windows.Forms.TextBox textR4;
+        private System.Windows.Forms.TextBox textR3;
+        private System.Windows.Forms.TextBox textR2;
+        private System.Windows.Forms.TextBox textR1;
+        private System.Windows.Forms.TextBox textG5;
+        private System.Windows.Forms.TextBox textG4;
+        private System.Windows.Forms.TextBox textG3;
+        private System.Windows.Forms.TextBox textG2;
+        private System.Windows.Forms.TextBox textG1;
+        private System.Windows.Forms.TextBox textZ5;
+        private System.Windows.Forms.TextBox textZ4;
+        private System.Windows.Forms.TextBox textZ3;
+        private System.Windows.Forms.TextBox textZ2;
+        private System.Windows.Forms.TextBox textZ1;
+        private System.Windows.Forms.Button bPlay;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox isvedimas;
+    }
+}
+
---
+++ _2018_03_16_pirma_uzduotis/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2018_03_16_pirma_uzduotis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox20_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox21_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox22_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox23_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textZ1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            
+            List<int> M = generuoja_kamuoliukus(1,16,5);
+            List<int> J = generuoja_kamuoliukus(16, 31,5);
+            List<int> R = generuoja_kamuoliukus(31, 46,5);
+            List<int> G = generuoja_kamuoliukus(46, 61,5);
+            List<int> Z = generuoja_kamuoliukus(61, 76,5);
+
+            int[] masyvo = M.ToArray();
+            textM1.Text = Convert.ToString(masyvo[0]);
+            textM2.Text = Convert.ToString(masyvo[1]);
+            textM3.Text = Convert.ToString(masyvo[2]);
+            textM4.Text = Convert.ToString(masyvo[3]);
+            textM5.Text = Convert.ToString(masyvo[4]);
+
+            int[] masyJ = J.ToArray();
+            textJ1.Text = Convert.ToString(masyJ[0]);
+            textJ2.Text = Convert.ToString(masyJ[1]);
+            textJ3.Text = Convert.ToString(masyJ[2]);
+            textJ4.Text = Convert.ToString(masyJ[3]);
+            textJ5.Text = Convert.ToString(masyJ[4]);
+
+            int[] masyvoR = R.ToArray();
+            textR1.Text = Convert.ToString(masyvoR[0]);
+            textR2.Text = Convert.ToString(masyvoR[1]);
+            textR3.Text = Convert.ToString(masyvoR[2]);
+            textR4.Text = Convert.ToString(masyvoR[3]);
+            textR5.Text = Convert.ToString(masyvoR[4]);
+
+            int[] masyvoG = G.ToArray();
+            textG1.Text = Convert.ToString(masyvoG[0]);
+            textG2.Text = Convert.ToString(masyvoG[1]);
+            textG3.Text = Convert.ToString(masyvoG[2]);
+            textG4.Text = Convert.ToString(masyvoG[3]);
+            textG5.Text = Convert.ToString(masyvoG[4]);
+
+            int[] masyvoZ = Z.ToArray();
+            textZ1.Text = Convert.ToString(masyvoZ[0]);
+            textZ2.Text = Convert.ToString(masyvoZ[1]);
+            textZ3.Text = Convert.ToString(masyvoZ[2]);
+            textZ4.Text = Convert.ToString(masyvoZ[3]);
+            textZ5.Text = Convert.ToString(masyvoZ[4]);
+        }
+
+
+        //kuriam metoda
+        private List<int> generuoja_kamuoliukus(int a, int b,int kiekis)
+        {
+            List<int> darbui = new List<int>();
+            Random rng = new Random();
+            
+            int ieskau = kiekis;
+
+            while (ieskau != 0)
+            {
+                int temp = rng.Next(a, b);
+                int flag = 0;
+                foreach (int kam in darbui)
+                {
+                    if (kam == temp)
+                    {
+                        flag++; //kartojasi kamuoliukas
+                    }
+                }
+                if (flag == 0)
+                {
+                    darbui.Add(temp); //nesikartoja
+                    ieskau--;
+                }
+            }
+            return darbui;
+
+        }
+
+        private void bPlay_Click(object sender, EventArgs e)
+        {
+            List<int> zaidimas = generuoja_kamuoliukus(1, 76, 49); //auksinis kamuoliukas 49tas
+
+           // isvedimas.Text = zaidimas.First().ToString();
+            foreach (int temp in zaidimas)
+            {
+                chec_if_number(temp);
+                isvedimas.Text += " " + temp.ToString();
+            }
+            
+        }
+
+        //kuriam metoda kuris patikrintu skaicius ir nuspalvintu atspetus skaicius
+        private void chec_if_number(int sk)
+        {
+            if (sk<16)
+            {
+                if (sk.ToString()==textM1.Text)
+                {
+                    textM1.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textM2.Text)
+                {
+                    textM2.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textM3.Text)
+                {
+                    textM3.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textM4.Text)
+                {
+                    textM4.BackColor = Color.Green;
+                }
+            }
+            else if (sk>15&&sk<31)
+            {
+                if (sk.ToString() == textJ1.Text)
+                {
+                    textJ1.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textJ2.Text)
+                {
+                    textJ2.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textJ3.Text)
+                {
+                    textJ3.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textJ4.Text)
+                {
+                    textJ4.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textJ5.Text)
+                {
+                    textJ5.BackColor = Color.Green;
+                }
+            }
+
+            else if (sk>30&&sk<46)
+            {
+                if (sk.ToString() == textR1.Text)
+                {
+                    textR1.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textR2.Text)
+                {
+                    textR2.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textR3.Text)
+                {
+                    textR3.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textR4.Text)
+                {
+                    textR4.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textR5.Text)
+                {
+                    textR5.BackColor = Color.Green;
+                }
+            }
+
+            else if (sk>45&&sk<61)
+            {
+                if (sk.ToString() == textG1.Text)
+                {
+                    textG1.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textG2.Text)
+                {
+                    textG2.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textG3.Text)
+                {
+                    textG3.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textG4.Text)
+                {
+                    textG4.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textG5.Text)
+                {
+                    textG5.BackColor = Color.Green;
+                }
+            }
+
+            else if (sk>60&&sk<76)
+            {
+                if (sk.ToString() == textZ1.Text)
+                {
+                    textZ1.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textZ2.Text)
+                {
+                    textZ2.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textZ3.Text)
+                {
+                    textZ3.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textZ4.Text)
+                {
+                    textZ4.BackColor = Color.Green;
+                }
+                if (sk.ToString() == textZ5.Text)
+                {
+                    textZ5.BackColor = Color.Green;
+                }
+            }
+        }
+    }     
+}
